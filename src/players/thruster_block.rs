53357c136fad578352188aa52ebd0b8bd54b51e1
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-11-19 10:15:11 +1000

    feat: controllable acceleration

--- src/players/thruster_block.rs
+++ src/players/thruster_block.rs
@@ -42,53 +42,56 @@ impl ThrusterPlugin {
 			commands.entity(thruster).with_children(|parent| {
 				debug!("Spawning thruster visuals");
 
+				/// Can change, typically stays constant
 				const ACCELERATION_ATTR: &str = "dynamic_accel";
+				/// Makes a visual difference in the colour and range of the particles.
+				/// Between 0 (for no lifetime) and 1 (for full lifetime)
+				const LIFETIME_ATTR: &str = "dynamic_lifetime";
 
 				let mut color_gradient = Gradient::new();
 				color_gradient.add_key(0.0, Vec4::splat(1.0));
-				color_gradient.add_key(0.1, Vec4::new(1.0, 1.0, 0.0, 1.0));
-				color_gradient.add_key(0.4, Vec4::new(1.0, 0.0, 0.0, 1.0));
-				color_gradient.add_key(1.0, Vec4::splat(0.0));
+				color_gradient.add_key(0.4, Vec4::new(1.0, 1.0, 0.0, 1.0));
+				color_gradient.add_key(0.7, Vec4::new(1.0, 0.0, 0.0, 1.0));
+				color_gradient.add_key(1.0, Vec4::new(0.2, 0., 0., 1.));
 
 				let mut size_gradient = Gradient::new();
 				size_gradient.add_key(0.0, Vec2::splat(0.1));
 				size_gradient.add_key(0.5, Vec2::splat(0.5));
-				size_gradient.add_key(0.8, Vec2::splat(0.08));
-				size_gradient.add_key(1.0, Vec2::splat(0.0));
+				size_gradient.add_key(1.0, Vec2::splat(0.08));
 
-				let writer1 = ExprWriter::new();
+				let writer = ExprWriter::new();
 
-				let age = writer1.lit(0.).expr();
+				let age = (writer.lit(1.) - writer.prop(LIFETIME_ATTR)).expr();
 				let init_age1 = SetAttributeModifier::new(Attribute::AGE, age);
 
-				let lifetime = writer1.lit(0.3).expr();
+				let lifetime = writer.lit(1.).expr();
 				let init_lifetime1 = SetAttributeModifier::new(Attribute::LIFETIME, lifetime);
-
 
 				// Add constant downward acceleration to simulate gravity
 				// let accel1 = writer1.lit(Vec3::Y * -3.).expr();
 				// let update_accel1 = AccelModifier::new(accel1);
 
 				let init_pos1 = SetPositionCone3dModifier {
-					base_radius: writer1.lit(PIXEL_SIZE * 0.1).expr(),
-					top_radius: writer1.lit(PIXEL_SIZE * 0.7).expr(),
-					height: writer1.lit(PIXEL_SIZE * 2.).expr(),
+					base_radius: writer.lit(PIXEL_SIZE * 0.1).expr(),
+					top_radius: writer.lit(PIXEL_SIZE * 0.7).expr(),
+					height: writer.lit(PIXEL_SIZE * 2.).expr(),
 					dimension: ShapeDimension::Volume,
 				};
 
 				let init_vel1 = SetVelocitySphereModifier {
-					center: writer1.lit(Vec3::ZERO).expr(),
-					speed: writer1.prop(ACCELERATION_ATTR).expr(),
+					center: writer.lit(Vec3::ZERO).expr(),
+					speed: writer.prop(ACCELERATION_ATTR).expr(),
 				};
 
 				let effect = effects.add(
 					EffectAsset::new(
 						32768,
 						Spawner::rate(500.0.into()).with_starts_active(true),
-						writer1.finish(),
+						writer.finish(),
 					)
 					.with_name("emit:rate")
-    .with_property(ACCELERATION_ATTR, PIXEL_SIZE * 10.)
+					.with_property(ACCELERATION_ATTR, Value::from(PIXEL_SIZE * 10.))
+					.with_property(LIFETIME_ATTR, Value::from(0.5))
 					// .with_property("my_accel", Vec3::new(0., -3., 0.).into())
 					.init(init_pos1)
 					// Make spawned particles move away from the emitter origin
