75d9fc7f35cd4cd82c65a4a0da900a07dcba5807
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-11-27 09:37:26 +1000

    add: building for macos

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
 
@@ -16,6 +17,9 @@ enum Cli {
 struct Release {
 	#[command(subcommand)]
 	platform: Platform,
+
+	#[arg(long, short, default_value_t = get_bin_name())]
+	bin_name: String,
 }
 
 #[derive(clap::Args)]
@@ -36,20 +40,45 @@ struct Setup {
 enum Platform {
 	Windows,
 	MacOS,
-	Web,
+	// Web,
+	// Linux,
+}
+
+fn get_cargo_path() -> String {
+	// get cargo executable from env CARGO, and run it with str
+	let cargo_exec_path = std::env::var("CARGO").unwrap();
+	assert!(Path::new(&cargo_exec_path).is_file());
+	cargo_exec_path
 }
 
 fn cargo_exec<'s>(args: impl IntoIterator<Item = &'s str>) {
 	// get cargo executable from env CARGO, and run it with str
-	let cargo_exec_path = std::env::var("CARGO").unwrap();
+	let cargo_exec_path = get_cargo_path();
 	exec(&cargo_exec_path, args);
 }
 
-fn exec<'a, 's>(exec_str: &'a str, args: impl IntoIterator<Item = &'s str>) {
+fn exec<'a, 's>(exec_str: &'a str, args: impl IntoIterator<Item = &'s str>) -> String {
 	let mut exec = std::process::Command::new(exec_str);
 	exec.args(args);
+	let exec_output = exec.spawn().unwrap().wait_with_output().unwrap();
+	assert!(exec_output.status.success());
+	exec_output
+		.stdout
+		.into_iter()
+		.map(|b| b as char)
+		.collect::<String>()
+}
+
+fn exec_with_envs<'a, 's>(
+	exec_str: &'a str,
+	args: impl IntoIterator<Item = &'s str>,
+	envs: impl IntoIterator<Item = (&'s str, &'s str)>,
+) {
+	let mut exec = std::process::Command::new(exec_str);
+	exec.args(args);
+	exec.envs(envs);
 	let exec_output = exec.spawn().unwrap().wait().unwrap();
-	assert!(exec_output.success())
+	assert!(exec_output.success());
 }
 
 fn get_bin_name() -> String {
@@ -60,20 +89,71 @@ fn get_bin_name() -> String {
 	bin_name.to_string()
 }
 
+#[cfg(target_os = "macos")]
+fn get_sdk_root() -> PathBuf {
+	let str = exec("xcrun", ["-sdk", "macosx", "--show-sdk-path"]);
+	let str = str.trim();
+	let path = Path::new(str);
+
+	assert!(path.exists());
+
+	PathBuf::from(path)
+}
+
 fn main() {
 	let args = Cli::parse();
 
 	match args {
-		Cli::Release(Release { platform }) => match platform {
+		Cli::Release(Release { platform, bin_name }) => match platform {
 			Platform::Windows => {
-				cargo_exec(["build", "--release", "--target", "x86_64-pc-windows-gnu", "--no-default-features", "--features", "release"]);
+				cargo_exec([
+					"build",
+					"--release",
+					"--target",
+					"x86_64-pc-windows-gnu",
+					"--no-default-features",
+					"--features",
+					"release",
+				]);
 				assert!(Path::new("target/x86_64-pc-windows-gnu/release/").is_dir());
-				let bin_name = get_bin_name();
-				assert!(Path::new(format!("target/x86_64-pc-windows-gnu/release/{}.exe", bin_name).as_str()).is_file());
+				assert!(Path::new(
+					format!("target/x86_64-pc-windows-gnu/release/{}.exe", bin_name).as_str()
+				)
+				.is_file());
 
 				todo!("Package windows build");
 			}
-			_ => todo!(),
+			Platform::MacOS => {
+				let sdk_root = get_sdk_root();
+				let sdk_root = sdk_root.to_str().unwrap();
+				exec_with_envs(
+					&get_cargo_path(),
+					[
+						"build",
+						"--release",
+						"--no-default-features",
+						"--features",
+						"release",
+						"--target=aarch64-apple-darwin",
+					],
+					[("SDKROOT", sdk_root)],
+				);
+				let silicon_build = format!("target/aarch64-apple-darwin/release/{bin_name}");
+				let silicon_build = Path::new(silicon_build.as_str());
+				assert!(silicon_build.is_file());
+				exec_with_envs(
+					&get_cargo_path(),
+					[
+						"build",
+						"--release",
+						"--no-default-features",
+						"--features",
+						"release",
+						"--target=x86_64-apple-darwin",
+					],
+					[("SDKROOT", sdk_root)],
+				);
+			}
 		},
 		Cli::Setup(Setup {
 			platform,
@@ -98,7 +178,10 @@ fn main() {
 				#[cfg(target_os = "macos")]
 				exec("brew", ["install", "mingw-w64"]);
 			}
-			_ => todo!(),
+			Platform::MacOS => {
+				exec("rustup", ["target", "add", "aarch64-apple-darwin"]);
+				exec("rustup", ["target", "add", "x86_64-apple-darwin"]);
+			}
 		},
 		Cli::Update => {
 			cargo_exec(["update"]);
