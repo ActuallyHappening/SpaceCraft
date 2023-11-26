7d71d2b2f0339d5f629d0663c3d4449f0d3541ac
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-11-26 21:15:47 +1000

    _

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -6,6 +6,7 @@ use clap::{Parser, Subcommand};
 enum Cli {
 	Release(Release),
 	Dev(Dev),
+	Setup(Setup),
 }
 
 #[derive(clap::Args)]
@@ -20,6 +21,15 @@ struct Dev {
 	platform: Platform,
 }
 
+#[derive(clap::Args)]
+struct Setup {
+	#[command(subcommand)]
+	platform: Platform,
+
+	#[arg(long, short)]
+	user_name: String,
+}
+
 #[derive(Subcommand)]
 enum Platform {
 	Windows,
@@ -27,6 +37,51 @@ enum Platform {
 	Web,
 }
 
+fn cargo_exec<'s>(args: impl IntoIterator<Item = &'s str>) {
+	// get cargo executable from env CARGO, and run it with str
+	let cargo_exec_path = std::env::var("CARGO").unwrap();
+	exec(&cargo_exec_path, args);
+}
+
+fn exec<'a, 's>(exec_str: &'a str, args: impl IntoIterator<Item = &'s str>) {
+	let mut exec = std::process::Command::new(exec_str);
+	exec.args(args);
+	let exec_output = exec.spawn().unwrap().wait().unwrap();
+	assert!(exec_output.success())
+}
+
 fn main() {
-	let _args = Cli::parse();
+	let args = Cli::parse();
+
+	match args {
+		Cli::Release(Release { platform }) => match platform {
+			Platform::Windows => {
+				cargo_exec(["build", "--release", "--target", "x86_64-pc-windows-gnu"]);
+			}
+			_ => todo!(),
+		},
+		Cli::Setup(Setup {
+			platform,
+			user_name,
+		}) => match platform {
+			Platform::Windows => {
+				exec("rustup", ["target", "add", "x86_64-pc-windows-msvc"]);
+				cargo_exec(["install", "xwin"]);
+				exec(
+					"xwin",
+					[
+						"--accept-license",
+						"splat",
+						"--disable-symlinks",
+						"--output",
+						format!("/Users/{}/.xwin", user_name).as_str(),
+					],
+				);
+				exec("brew", ["install", "llvm"]);
+				exec("brew", ["install", "mingw-w64"]);
+			}
+			_ => todo!(),
+		},
+		_ => todo!(),
+	}
 }
