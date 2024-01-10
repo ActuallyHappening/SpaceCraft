f5e0bc99122dbb2a687bcea5402103e17b2f5440
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-01-10 19:43:33 +0900

    todo

--- crates/bevy_xpbd3d_thrusters/src/lib.rs
+++ crates/bevy_xpbd3d_thrusters/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-	left + right
+pub mod prelude {
+	pub(crate) use bevy::{prelude::*, utils::HashMap};
 }
 
-#[cfg(test)]
-mod tests {
-	use super::*;
+mod old_types {
+	use bevy::prelude::*;
 
-	#[test]
-	fn it_works() {
-		let result = add(2, 2);
-		assert_eq!(result, 4);
+	/// Forces are between [-1, 1],
+	/// Torques are 'normalized' to [-1, 1] // TODO
+	///
+	/// The Greek philosopher, Archimedes, said,
+	/// “Give me a lever long enough and a fulcrum on which to place it, and I shall move the world.”
+	#[derive(Debug, Reflect, Default, Clone, Copy)]
+	pub struct ForceAxis {
+		forward: f32,
+		right: f32,
+		upwards: f32,
+		turn_right: f32,
+		pitch_up: f32,
+		roll_right: f32,
+	}
+}
+
+mod strategies {
+	use crate::prelude::*;
+
+	#[derive(Debug, Hash, Clone, PartialEq, PartialOrd, Eq, Ord, Reflect)]
+	pub struct ID(u64);
+
+	#[reflect_trait]
+	pub trait Strategy {
+		fn calculate(&self, blocks: HashMap<&ID>)
 	}
 }
--- src/physics.rs
+++ src/physics.rs
@@ -6,7 +6,7 @@ impl Plugin for PhysicsPlugin {
 	fn build(&self, app: &mut App) {
 		app.add_plugins((
 			PhysicsPlugins::new(FixedUpdate),
-			bevy_xpbd3d_parenting::PhysicsParentingPlugin,
+			bevy_xpbd3d_parenting::PhysicsParentingPlugin::default(),
 		));
 		#[cfg(feature = "debug")]
 		app.add_plugins(PhysicsDebugPlugin::default());
